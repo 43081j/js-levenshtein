a8b3238d4fa7b664b538be09fcbe8697779793ba
Michael Stramel <m.stramel89@gmail.com>
2019-02-21 12:12:01 -0600

    Clean up performance testing

---
+++ perf/data/index.js
@@ -0,0 +1,3 @@
+export {default as paragraphs} from './paragraphs';
+export {default as sentences} from './sentences';
+export {default as words} from './words';
---
+++ perf/index.js
@@ -0,0 +1,63 @@
+const levenshteinEditDistance = require('levenshtein-edit-distance');
+const fastLevenshtein = require('fast-levenshtein');
+const jsLevenshtein = require('js-levenshtein');
+const talisman = require('talisman/metrics/distance/levenshtein');
+const leven = require('leven');
+const jsLevenshteinEsm = require('../');
+const {paragraphs, sentences, words} = require('esm')(module)('./data');
+
+if (!Object.entries) {
+  Object.entries = function( obj ) {
+    const ownProps = Object.keys( obj );
+    let i = ownProps.length;
+    const resArray = new Array(i); // preallocate the Array
+    while (i--) {
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    }
+    return resArray;
+  };
+}
+
+/**
+ * Executes a given function across the given dataset
+ *
+ * @param {string[]} data Dataset to execute against
+ * @return {Function} - bench runner
+ */
+function getBenchRunner(data) {
+  return (fn) => {
+    for (let i = 0; i + 1 < data.length; i += 2) {
+      const p1 = data[i];
+      const p2 = data[i + 1];
+      fn(p1, p2);
+    }
+  };
+}
+
+const paragraphBench = getBenchRunner(paragraphs);
+const sentenceBench = getBenchRunner(sentences);
+const wordBench = getBenchRunner(words);
+
+const suites = Object.entries({
+  '50 paragraphs, length max=500 min=240 avr=372.5': paragraphBench,
+  '100 sentences, length max=170 min=6 avr=57.5': sentenceBench,
+  '2000 words, length max=20 min=3 avr=9.5': wordBench,
+});
+
+const libs = Object.entries({
+  'fast-levenshtein': fastLevenshtein.get,
+  'js-levenshtein': jsLevenshtein,
+  'js-levenshtein-esm': jsLevenshteinEsm.default,
+  leven,
+  'levenshtein-edit-distance': levenshteinEditDistance,
+  talisman,
+});
+
+// Run each lib in each suite
+for (const [suiteName, benchRunner] of suites) {
+  suite(suiteName, () => {
+    for (const [libName, fn] of libs) {
+      bench(libName, () => benchRunner(fn));
+    }
+  });
+}
